f91f1c6d694bee920f0353e0c33e237ad908ae8a
catanzo96 <giacomocatanzaro@hotmail.it>
2022-06-03 18:11:21 +0200

    Add first elements and error handling

--- js/script.js
+++ js/script.js
@@ -1,64 +1,49 @@
 const inputCity = document.querySelector('.city-input');
 const searchButton = document.querySelector('.search');
 
-function showCity() {
-  let url = createURL(checkInput());
-  fetchData(url);
-}
 
-searchButton.addEventListener('click', function() {
-  showCity();
-});
-
-//////////////////////////////////////////////////////////////////////////////// Funzione controllo input
-function checkInput() {
-
-    if (inputCity.value == '' || !isNaN(inputCity.value)) {
-      console.log('errore da gestire');
-    } else {
-      console.log(inputCity.value);
-      return inputCity.value;
-    };
-
-    inputCity.value = '';
+/////////////////////////////////////////////////////////////// Funzione wrapper
+function CityLifeStyle() {
+  fetchData(getURL(inputCity.value));
 };
-/////////////////////////////////////////////////////////////////// Funzione che genera l'url dall'input
-function createURL(name) {
-  let low = name.toLowerCase();
-  let str = low.replaceAll(' ', '-');
+
+////////////////////////////////////////////////////// Funzione per generare URL
+function getURL(name) {
+  let str = (name.toLowerCase()).replaceAll(' ', '-');
   let url = `https://api.teleport.org/api/urban_areas/slug:${str}/scores/`;
-  console.log(url);
   return url;
 };
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Carichiamo l'oggetto con le informazioni
+
+///////////////////////////////////// Funzione per controllo e chiamata dei dati
 async function fetchData(url) {
   const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
-  createResult(data)
+  if (!response.ok) {
+    createError();
+  } else {
+    const data = await response.json();
+    console.log(data)
+  }
 };
 
+////////////////////////////////////////////////// Funzione per gestire l'errore
+const mainMenu = document.querySelector('.menu');
+const errorDiv = document.createElement('div');
+const errorMessage = document.createElement('p');
 
+function createError() {
+  errorDiv.classList.add('col-12', 'error-message');
+  errorMessage.innerHTML = 'Errore!';
+  errorDiv.append(errorMessage);
+  mainMenu.append(errorDiv);
+};
 
-function createResult(data) {
-  const par = document.createElement('p');
-  par.innerHTML = data.summary;
-  summaryDiv.append(par)
-
-  const score = document.createElement('h3');
-  score.innerHTML = (data.teleport_city_score).toFixed(2);
-  scoreDiv.append(score);
-
-  data.categories.forEach(category => {
-    let cat = document.createElement('p');
-    cat.textContent = `${category.name}: ${(category.score_out_of_10).toFixed(2)}/10`
-    categoriesDiv.append(cat);
-  })
+/////////////////////////////////////////////////// Funzione per ripulire output
+function clearOutput() {
+  errorDiv.remove();
 }
 
-const summaryDiv = document.querySelector('.summary');
-const categoriesDiv = document.querySelector('.categories');
-const scoreDiv = document.querySelector('.score');
 
-/////
+searchButton.addEventListener('click', function() {
+  clearOutput();
+  CityLifeStyle();
+});
